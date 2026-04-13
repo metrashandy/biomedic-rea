2a9b260fe576de4baf238de51c791c24f58dcea4
MasDana <putudeni1414@gmail.com>
2026-04-13 19:47:45 +0800

    menambahkan segmentasi

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import re
 import json
-import torch
 import numpy as np
 import cv2
 import base64
@@ -11,14 +10,68 @@ from dotenv import load_dotenv
 from typing import Optional
 from deep_translator import GoogleTranslator
 from openai import OpenAI
+from PIL import Image
+import io
 
-# ================== IMPORT HUGGING FACE ==================
-from transformers import AutoModel
+def to_base64(image):
+    _, buffer = cv2.imencode('.jpg', image)
+    return base64.b64encode(buffer).decode('utf-8')
+
+def refine_bbox_with_opencv(image, bboxes):
+    refined_boxes = []
+
+    h_img, w_img, _ = image.shape
+
+    for bbox in bboxes:
+        # convert normalized → pixel
+        x = int(bbox["x"] * w_img)
+        y = int(bbox["y"] * h_img)
+        w = int(bbox["width"] * w_img)
+        h = int(bbox["height"] * h_img)
+
+        roi = image[y:y+h, x:x+w]
+
+        if roi.size == 0:
+            continue
+
+        # grayscale
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+        # threshold (area putih)
+        gray = cv2.GaussianBlur(gray, (5,5), 0)
+
+        thresh = cv2.adaptiveThreshold(
+            gray,
+            255,
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY,
+            11,
+            2
+        )
+
+        # hilangkan noise
+        kernel = np.ones((5,5), np.uint8)
+        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+        # cari kontur
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cnt in contours:
+            if cv2.contourArea(cnt) > 300:
+                x2, y2, w2, h2 = cv2.boundingRect(cnt)
+
+                refined_boxes.append((
+                    x + x2,
+                    y + y2,
+                    w2,
+                    h2
+                ))
+
+    return refined_boxes
 
 # ================== LOAD ENV ==================
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-print("API KEY:", os.getenv("OPENAI_API_KEY"))
 
 # ================== INIT APP ==================
 app = FastAPI()
@@ -31,19 +84,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ================== LOAD HUGGING FACE MODEL ==================
-# Ini akan otomatis mendownload model saat server pertama kali jalan
-print("Loading Hugging Face Model...")
-device = "cuda" if torch.cuda.is_available() else "cpu"
-hf_model = AutoModel.from_pretrained("ianpan/chest-x-ray-basic", trust_remote_code=True)
-hf_model = hf_model.eval().to(device)
-hf_model = AutoModel.from_pretrained(
-    "ianpan/chest-x-ray-basic",
-    trust_remote_code=True,
-    ignore_mismatched_sizes=True
-)
-print("Model loaded successfully!")
-
 # ================== FUNGSI BANTUAN ==================
 def translate_text(text):
     if not text: return text
@@ -55,87 +95,19 @@ def translate_to_english(text):
     try: return GoogleTranslator(source='auto', target='en').translate(text)
     except: return text
 
-# Fungsi kalkulasi CTR bawaan dari Hugging Face
-def calculate_ctr(mask): 
-    lungs = np.zeros_like(mask)
-    lungs[mask == 1] = 1
-    lungs[mask == 2] = 1
-    heart = (mask == 3).astype("int")
-    
-    try:
-        y, x = np.stack(np.where(lungs == 1))
-        lung_min, lung_max = x.min(), x.max()
-        y, x = np.stack(np.where(heart == 1))    
-        heart_min, heart_max = x.min(), x.max()
-        lung_range = lung_max - lung_min
-        heart_range = heart_max - heart_min
-        return float(heart_range / lung_range)
-    except ValueError:
-        return 0.0 # Return 0 jika gagal menemukan paru/jantung
+def draw_boxes(image, boxes):
+    img = image.copy()
 
-# ================== FUNGSI PROSES HUGGING FACE ==================
-def process_huggingface_image(image_bytes):
-    # 1. Decode gambar dari FastApi ke format OpenCV (Grayscale)
-    np_arr = np.frombuffer(image_bytes, np.uint8)
-    img = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
-    
-    # Simpan gambar asli (berwarna) untuk background overlay nanti
-    img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    for (x, y, w, h) in boxes:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-    # 2. Preprocess untuk model HF
-    x = hf_model.preprocess(img) 
-    x = torch.from_numpy(x).unsqueeze(0).unsqueeze(0).float()
-
-    # 3. Jalankan Model AI
-    with torch.inference_mode():
-        out = hf_model(x.to(device))
-
-    # 4. Ambil data teks (Umur, Gender, View)
-    age = float(out["age"].item())
-    is_female = bool(out["female"].item() >= 0.5)
-    view_idx = out["view"].argmax(1).item()
-    view_map = {0: "AP", 1: "PA", 2: "Lateral"}
-    view_str = view_map.get(view_idx, "Unknown")
-
-    # 5. Ambil gambar MASK (Segmentasi)
-    mask = out["mask"].argmax(1).squeeze().cpu().numpy()
-    
-    # 6. Hitung CTR
-    ctr_value = calculate_ctr(mask)
-
-    # 7. Mewarnai Mask (Biar keren di Frontend!)
-    # Resize mask agar ukurannya sama persis dengan gambar asli
-    mask_resized = cv2.resize(mask.astype(np.uint8), (img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST)
-    
-    colored_mask = np.zeros_like(img_color)
-    # Paru Kanan = Biru muda
-    colored_mask[mask_resized == 1] = [255, 191, 0] # BGR
-    # Paru Kiri = Hijau
-    colored_mask[mask_resized == 2] =[0, 255, 0]
-    # Jantung = Merah
-    colored_mask[mask_resized == 3] = [0, 0, 255]
-
-    # Gabungkan gambar asli dengan warna (Alpha blending/Transparan)
-    overlay = cv2.addWeighted(img_color, 0.7, colored_mask, 0.4, 0)
-
-    # 8. Convert overlay ke Base64 untuk dikirim ke frontend
-    _, buffer = cv2.imencode(".png", overlay)
-    segmented_base64 = base64.b64encode(buffer).decode("utf-8")
-
-    return {
-        "image_base64": segmented_base64,
-        "age": round(age, 1),
-        "gender": "Perempuan" if is_female else "Laki-laki",
-        "view": view_str,
-        "ctr": round(ctr_value, 2)
-    }
+    return img
 
 # ================== MAIN ENDPOINT ==================
 @app.post("/analyze")
 async def analyze_xray(
     image: UploadFile = File(...),
     symptoms: Optional[str] = Form(None)
-    
 ):
     if not os.getenv("OPENAI_API_KEY"):
         raise HTTPException(status_code=500, detail="OpenAI API key tidak ditemukan")
@@ -145,38 +117,50 @@ async def analyze_xray(
         raise HTTPException(status_code=400, detail="File harus JPG atau PNG")
 
     contents = await image.read()
+
     if len(contents) > 100 * 1024 * 1024:
         raise HTTPException(status_code=400, detail="Ukuran file maksimal 100MB")
 
-    # ================== 1. PROSES LOKAL (HUGGING FACE) ==================
-    hf_data = process_huggingface_image(contents)
+    # ================== LOAD IMAGE ==================
+    pil_image = Image.open(io.BytesIO(contents)).convert("RGB")
 
-    # ================== 2. REQUEST KE OPENAI ==================
-
-
-    # encode image ke base64
+    # ================== SEGMENTASI (UNET KAGGLE) ==================
+    #base64_img = to_base64(overlay)
+    
+    # ================== OPENAI ==================
     image_base64 = base64.b64encode(contents).decode("utf-8")
 
-    translated_symptoms = translate_to_english(symptoms) if symptoms else "None"
+    prompt = """
+    Analyze this chest X-ray image. Focus only on visible patterns (opacity, asymmetry, density differences). Do NOT provide a medical diagnosis. Identify ALL suspicious regions if visible. Return multiple bounding boxes if needed. If no clear abnormality is visible, return empty list.
+    Identify ALL suspicious regions if visible.
 
-    prompt = f"""
-    Analyze this chest X-ray image and describe visible findings.
+    Each bounding box MUST:
+    - tightly fit the abnormal region
+    - avoid including healthy areas
+    - be as small as possible while covering the abnormality
 
-    Focus only on visual patterns. Do not provide a medical diagnosis.
+    Do NOT create large boxes covering the center unless absolutely necessary.
 
+Coordinates must be normalized between 0 and 1.
     Return STRICT JSON:
-
-    {{
+    {
     "findings": "...",
     "abnormality": "...",
     "risk": 0-100,
-    "recommendation": {{
+    "bboxes": [
+        {
+        "x": 0-1,
+        "y": 0-1,
+        "width": 0-1,
+        "height": 0-1
+        }
+    ],
+    "recommendation": {
         "approach": "...",
         "treatment": "..."
-    }}
-    }}
+    }
+    }
     """
-
     response = client.responses.create(
         model="gpt-5.4-mini",
         input=[
@@ -196,61 +180,54 @@ async def analyze_xray(
 
     try:
         content = response.output[0].content[0].text
-
         content = re.sub(r"```json|```", "", content).strip()
+
         print("RAW AI:", content)
+
         ai_result = json.loads(content)
 
         if "findings" not in ai_result:
             raise HTTPException(status_code=500, detail="Format AI tidak sesuai")
 
-    except json.JSONDecodeError:
-        print("RAW AI RESPONSE:", content)
-        raise HTTPException(status_code=500, detail="Format JSON dari AI tidak valid")
-
     except Exception as e:
         print("ERROR:", str(e))
-        raise HTTPException(status_code=500, detail="Terjadi kesalahan saat memproses AI")
-
-    # ================== 3. GABUNGKAN HASIL & TRANSLATE ==================
+        raise HTTPException(status_code=500, detail="Format AI tidak valid")
     
-    # Masukkan hasil segmentasi HF ke JSON response
-    data = {
-    "result": ai_result
-    }
-    data["segmentation_image"] = hf_data["image_base64"]
-    data["ai_metadata"] = {
-        "age": hf_data["age"],
-        "gender": hf_data["gender"],
-        "view": hf_data["view"],
-        "ctr_ratio": hf_data["ctr"]
-    }
+    # ================== SEGMENTASI ==================
+    #mask = predict_mask(pil_image)
+    #overlay = overlay_mask(pil_image, mask)
+    #overlay = np.array(pil_image)
+
+    # ================== BBOX ==================
+    bboxes = ai_result.get("bboxes", [])
     
-    try:
-        if "result" in data:
-            analysis = data["result"].get("analysis", {})
-            analysis["findings"] = translate_text(analysis.get("findings"))
-            analysis["potential_abnormalities"] = translate_text(analysis.get("potential_abnormalities"))
-            analysis["observations"] = translate_text(analysis.get("observations"))
+    if not bboxes:
+        print("AI tidak mendeteksi area abnormal")
 
-            risk = data["result"].get("risk_assessment", {})
-            risk["assessment_explanation"] = translate_text(risk.get("assessment_explanation"))
+    overlay = np.array(pil_image)
 
-            technical = data["result"].get("technical_assessment", {})
-            technical["positioning"] = translate_text(technical.get("positioning"))
-            technical["exposure"] = translate_text(technical.get("exposure"))
-            technical["artifacts"] = translate_text(technical.get("artifacts"))
+    refined_boxes = refine_bbox_with_opencv(overlay, bboxes)
 
-            data["result"]["specific_response"] = translate_text(data["result"].get("specific_response"))
+    if refined_boxes:
+        overlay = draw_boxes(overlay, refined_boxes)
+    else:
+        print("Fallback ke bbox AI")
 
-            treatment = data["result"].get("treatment_recommendations", {})
-            treatment["general_approach"] = translate_text(treatment.get("general_approach"))
-            treatment["possible_treatments"] = translate_text(treatment.get("possible_treatments"))
-            treatment["follow_up"] = translate_text(treatment.get("follow_up"))
+        h, w, _ = overlay.shape
 
-            data["result"]["recommendations"] = translate_text(data["result"].get("recommendations"))
-            data["result"]["disclaimer"] = translate_text(data["result"].get("disclaimer"))
-    except Exception as e:
-        print("Translate error:", e)
+        for bbox in bboxes:
+            x = int(bbox["x"] * w)
+            y = int(bbox["y"] * h)
+            bw = int(bbox["width"] * w)
+            bh = int(bbox["height"] * h)
 
-    return data
+            overlay = draw_boxes(overlay, [(x, y, bw, bh)])
+
+    # ================== FINAL ==================
+    base64_img = to_base64(overlay)
+
+    # ================== RETURN ==================
+    return {
+        "result": ai_result,
+        "segmentation_image": base64_img
+    }
